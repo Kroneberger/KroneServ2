2713e5263331ec8bb8141413fdf0a0428808c8e7
Kroneberger <hernankroneberger@gmail.com>
2026-08-09 21:07:01 -0300

    Add files via upload

---
+++ Pagina Principal/index.js
@@ -0,0 +1,12 @@
+/*const link_contacto = document.getElementById("link_contacto");
+
+
+
+
+function contacto(){
+    alert("Tocaste contacto");
+}
+
+link_contacto.addEventListener("click", function(){
+    contacto();
+})*/
